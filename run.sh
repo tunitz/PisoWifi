2b0d69a330ef035d42589e1f09fadb85d59b27f6
tunitz <122100577+tunitz@users.noreply.github.com>
2023-05-08 00:30:40 +0800

    Fix environment issue

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-/usr/bin/python3 /root/PisoWifi/coin_slot.py
+# Define the path to the Python script
+python_script="/root/PisoWifi/coin_slot.py"
+
+# Define the path to the virtual environment
+env_path="/root/PisoWifi/"
+
+# Activate the virtual environment
+source "$env_path/bin/activate"
+
+# Run the Python script using the activated environment
+/usr/bin/python3 "$python_script"
